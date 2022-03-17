72cbbbde19ace90adf64e74492b787085334a056
sshah <sshah@edubrite.com>
2022-03-17 17:37:39 +0530

    Recursion (Tower Of Hanoi )

--- JavaProject1/src/Recursion.java
+++ JavaProject1/src/Recursion.java
@@ -13,7 +13,7 @@ public class Recursion {
 		
 		towerOfHanoi(n-1, src, dest, helper);
 		System.out.println("transfer disk "+ n + " from "+src+" to "+dest);
-		towerOfHanoi(n-1,helper, src, dest);
+		towerOfHanoi(n-1,helper, src, dest); 
 		
 		
 	}
