649a824014440249c733c317d06abd2f71a50fff
Sakshi Shah <67853646+Sakshi-Shah5@users.noreply.github.com>
2022-08-26 09:01:40 +0530

    Create FirstUniqueChar.java

---
+++ FirstUniqueChar.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class FirstUniqueChar {
+	
+	public static char firstUniqChar(String str){
+		HashMap<Character,Integer> map = new HashMap<>();
+		
+		for(int i=0; i<str.length(); i++){
+			char ch = str.charAt(i);
+			 if(!map.containsKey(ch)){
+				 map.put(ch, 1);
+			 }
+			 else {
+				 map.put(ch, map.get(ch) + 1);
+			 }
+		  }
+		
+		  for(int i=0; i<str.length(); i++) {
+			  char ch = str.charAt(i);
+			  
+			  if(map.get(ch) == 1) {
+				  return ch;
+			  }
+			  
+			  
+		  }
+		  
+		  
+		  return 0; 
+		}
+	
+	public static void main(String[] args) {
+		String str = "sakshi";
+		System.out.print(firstUniqChar(str));
+		
+	}
+
+}
